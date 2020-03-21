2f5ca1b38e627a8939fef37620eda21afce99e84
Matthew Chartier <MChartier@gmail.com>
2020-03-21 15:18:38 -0700

    Integrating with MatterhornApiService dev environment (#1)
    Adding MatterhornApiClient class for connecting to MatterhornApiService.
    Adding logic to App.tsx to query 'counter' API on MatterhornApiService.
    Adding jest packages for unit testing, along with trivial unit test.
    Adding dev packages for linting typescript, along with tslint.json configuration.
    Adding .gitattributes file to control line endings across different platforms.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MatterhornApiClient, { CounterResponse } from './api-client/MatterhornApiClient';
+
+const apiBaseUrl = 'http://matterhornapiservice-env.eba-qjezc5kq.us-east-1.elasticbeanstalk.com';
+const apiClient: MatterhornApiClient = new MatterhornApiClient(apiBaseUrl);
 
 import getEnvVars from './environment';
 const { message } = getEnvVars();
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text>{message}</Text>
+      <Text>Counter: {count}</Text>
+      <Button onPress={() => retrieveCount(apiClient, setCount)} title="Click me"/>
     </View>
   );
 }
@@ -20,3 +28,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function retrieveCount(api: MatterhornApiClient, setCount: React.Dispatch<React.SetStateAction<number>>) {
+  api.Counter().then((value: CounterResponse) => {
+    setCount(value.Value);
+  }).catch((reason) => {
+    console.log('Request failed!');
+    console.log(reason);
+  });
+}
