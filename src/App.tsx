d2b8fb9f51b159da9a191ceb5a8c710a35c88155
Matthew Chartier <MChartier@gmail.com>
2020-04-01 13:34:01 -0700

    Adding authentication with Auth0 (#9)
    Adding authentication check before displaying regular navigation.
    If a user is not authenticated (and we have not overridden auth for
    local development), they will be redirected to the Auth0 sign in page.
    When redirected back, we will show the normal app experience.
    Adding dependency on native-base to get access to stock UI components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AuthSession, AppLoading, registerRootComponent } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
 import { RestfulProvider } from 'restful-react';
-import { registerRootComponent } from 'expo';
 
+import { Spinner, View } from 'native-base';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useFonts } from '@use-expo/font';
+
 import CounterScreen from './views/CounterScreen';
 import MainScreen from './views/MainScreen';
 import getEnvVars from './utils/environment';
 
-const { apiUrl } = getEnvVars();
+const {
+  apiUrl, auth0ClientId, auth0Domain, enableAuth
+} = getEnvVars();
 
 /**
  * To type check our route name and params, we need to create an object type
@@ -20,9 +25,51 @@ type RootNavParamList = {
   Counter: undefined;
 };
 
-const Tab = createMaterialTopTabNavigator<RootNavParamList>();
 
 function App() {
+  const [userToken, setUserToken] = useState<string>(enableAuth ? null : 'FAKE_TOKEN');
+  const [isLoggingIn, setIsLoggingIn] = useState<boolean>(false);
+
+  const [fontsLoaded] = useFonts({
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+  });
+
+  const signIn = async () => {
+    setIsLoggingIn(true);
+    const redirectUrl = encodeURIComponent(AuthSession.getRedirectUrl());
+    console.log(redirectUrl);
+    const authUrl = `${auth0Domain}/authorize?response_type=token&client_id=${auth0ClientId}&redirect_uri=${redirectUrl}&prompt=login`;
+    console.log(authUrl);
+    const result: any = await AuthSession.startAsync({ authUrl });
+    console.log(result);
+    setUserToken(result.params.access_token);
+    setIsLoggingIn(false);
+  };
+
+  useEffect(() => {
+    // Wait until font resources are loaded before redirecting to sign in.
+    // This helps avoid incorrect behavior when triggering the auth flow before
+    // AppState has initialized. (https://github.com/expo/expo/pull/6743)
+    if (fontsLoaded && !isLoggingIn && userToken === null) {
+      signIn();
+    }
+  });
+
+  const Tab = createMaterialTopTabNavigator<RootNavParamList>();
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  if (userToken === null) {
+    return (
+      <View>
+        <Spinner />
+      </View>
+    );
+  }
+
   return (
     <RestfulProvider base={apiUrl}>
       <NavigationContainer>
